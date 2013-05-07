727bf22bc1b2da497412c5709f57a41aaf63526b
fredteumer <frederick.teumer@tufts.edu>
2013-05-07 10:23:51 -0400

    working on tangents

--- js/init.js
+++ js/init.js
@@ -6,27 +6,29 @@ $(document).ready(function(){
 	init();
 });
 
+var c_x = 240;
+var c_y = 320;
+var C_radius = 50;
+
 function init(){
-	$('canvas').drawEllipse({
+	canvas.drawArc({
 		layer: true,
 		strokeStyle: "#000",
 		strokeWidth: 1.5,
 		x: 240, y: 320,
-		width: 100,
-		height: 100,
+		radius: 50,
 		name: 'C'
 	});
 	
-	$('canvas').drawEllipse({
+	canvas.drawArc({
 		layer: true,
 		fillStyle: "#000",
 		x: 240, y: 320,
-		width: 5,
-		height: 5,
+		radius: 2,
 		name: 'c'
 	});
 	
-	$('canvas').drawText({
+	canvas.drawText({
 		layer: true,
 		fillStyle: "#000",
 		strokeStyle: "#000",
@@ -38,7 +40,7 @@ function init(){
 		name: 'c-label'
 	});
 	
-	$('canvas').drawLine({
+	canvas.drawLine({
 		layer: true,
 		strokeStyle: "#000",
 		strokeWidth: 1.5,
@@ -47,7 +49,7 @@ function init(){
 		name: "l"
 	});
 	
-	$('canvas').drawText({
+	canvas.drawText({
 		layer: true,
 		fillStyle: "#000",
 		strokeStyle: "#000",
@@ -61,6 +63,68 @@ function init(){
 	
 }
 
+function findIntersection(l1, l2){}
+
+function Distance_Formula(x1, y1, x2, y2){
+	var d = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+	return d;
+}
+
+function findTangent(x,y){ //x and y are the coordinates of the ORANGE point
+	var dx = c_x - x;
+	var dy = c_y - y;
+	var dist = Distance_Formula(c_x, c_y, x, y);
+	var a = Math.asin(C_radius/dist);
+	var b = Math.atan2(dy, dx);
+	
+	t = b - a;
+	var ta = ta = { x:C_radius * Math.sin(t), y:C_radius * -Math.cos(t) };
+    
+    t = b + a
+    var tb = { x:C_radius * -Math.sin(t), y:C_radius * Math.cos(t) };
+    
+    return [ta, tb];
+}
+
+function drawTangent(x,y){
+	canvas.removeLayer("T1");
+	canvas.removeLayer("T2");
+
+	var l = findTangent(x,y);
+	
+	canvas.drawLine({
+		layer: true,
+		strokeStyle: "#000",
+		strokeWidth: 1.5,
+		x1: x, y1: y,
+		x2: l[0].x+c_x, y2: l[0].y+c_y,
+		x3: l[0].x+c_x, y3: l[0].y+c_y,
+		name: "T1"
+	});
+	
+	canvas.drawLine({
+		layer: true,
+		strokeStyle: "#000",
+		strokeWidth: 1.5,
+		x1: x, y1: y,
+		x2: l[1].x+c_x, y2: l[1].y+c_y,
+		x3: l[1].x+c_x, y3: l[1].y+c_y,
+		name: "T2"
+	});
+	
+	extendTangent(canvas.getLayer("T1"), canvas.getLayer("T2"));
+	
+}
+
+function extendTangent(T1, T2){
+	var y_bot = canvas.getLayer("l").y1;
+	T1.y3 = y_bot;
+	T2.y3 = y_bot;
+	
+	T1.x3 = T1.x2+(T1.x2-T1.x1);
+	T2.x3 = (((y_bot-T2.y2)(T2.x2-T2.x1)) / (T2.y2 - T2.y1)) + T2.x2
+}
+
 function setNumberOfSwimmers(numberOfSwimmers){
 	if(numberOfSwimmers==1){ beginOneSwimmer(); }
 	else if(numberOfSwimmers==2){ beginTwoSwimmers(); }
@@ -69,18 +133,26 @@ function setNumberOfSwimmers(numberOfSwimmers){
 	else{ alert("Something bad happened! Invalid number of swimmers!"); }
 }
 
+function beginOneSwimmer(){
+	alert('in 1 swimmer case! -- not finished yet!');
+}
+
+function beginTwoSwimmers(){
+	alert('in 2 swimmer case! -- not finished yet!');
+}
+
 function beginThreeSwimmers(){
 	
-	$('#swim1button').attr('disabled', false);
-	$('#swim2button').attr('disabled', false);
+	$('#swim1button').attr('disabled', true);
+	$('#swim2button').attr('disabled', true);
 	$('#swim3button').attr('disabled', true);
-	$('#swimkbutton').attr('disabled', false);
+	$('#swimkbutton').attr('disabled', true);
 	
 	$('#info').html("<p>In the case of 3 swimmers, each must cover an arc of the circle.</p> \
-		<p>Each swimmer is a color: <span class=\'red\'>Swimmer 1</span> is \
+		<p>Each swimmer has a color: <span class=\'red\'>Swimmer 1</span> is \
 		<span class=\'red\'>RED</span>, <span class=\'blu\'>Swimmer 2</span> is \
 		<span class=\'blu\'>BLUE</span>, and <span class=\'grn\'>Swimmer 3</span> is \
-		<span class=\'grn\'>GREEN</span></p> \
+		<span class=\'grn\'>GREEN</span>.</p> \
 		<p>The <span class=\'oj\'>ORANGE</span> point at the top can be dragged to designate \
 		the two tangents, T1 and T2.</p> \
 		<p>Try it out and see what is the optimal configuration for the three swimmers!</p>");
@@ -90,8 +162,14 @@ function beginThreeSwimmers(){
 		draggable: true,
 		cursor: "pointer",
 		fillStyle: "orange",
-		x: 240, y: 250,
-		radius: 5
-	})
-	//alert('in 3 swimmer case');
+		x: 240, y: 200,
+		radius: 7,
+		name: "p"
+	});
+	
+	drawTangent(canvas.getLayer("p").x, canvas.getLayer("p").y);
+}
+
+function beginkSwimmers(){
+	alert('in k swimmer case! -- not finished yet!');
 }
